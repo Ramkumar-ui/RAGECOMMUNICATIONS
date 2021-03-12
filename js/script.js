7007cebb69ebc989587ff246d6d4b8229cd20670
Ramkumar-ui <ramkumar2811993@gmail.com>
2021-03-12 14:20:36 +0530

    Task Completed

---
+++ js/script.js
@@ -0,0 +1,21 @@
+ // Perloader
+
+$(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+// Scroll Down
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
