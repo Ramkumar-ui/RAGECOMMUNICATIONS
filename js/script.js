546fd15f4be33e0dc50d546d2e692e5912e407d5
Ramkumar-ui <ramkumar2811993@gmail.com>
2021-03-13 10:27:43 +0530

    Changes Done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
- // Perloader
+// Perloader
 
 $(window).on('load', function () {
-    $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow');
+  $('#status').fadeOut();
+  $('#preloader').delay(350).fadeOut('slow');
 });
 
 // Scroll Down
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      } 
-    });
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
   });
+});
